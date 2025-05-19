83ba23dd794982c4a1b8352951789d79abc4f45b
SANTHEESH.S <santheeshcse2020@ksrce.ac.in>
2025-05-19 11:57:43 +0530

    update

--- server/config/db.js
+++ server/config/db.js
@@ -5,8 +5,8 @@ const connectDB = async () => {
     mongoose.set('strictQuery', false);
     
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
       serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     });
