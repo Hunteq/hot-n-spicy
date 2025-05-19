2d8a66cc9f9e0298ff84716d531861603a97971a
SANTHEESH.S <santheeshcse2020@ksrce.ac.in>
2025-05-19 11:38:30 +0530

    update

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
-  
   try {
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    });
+    
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.error('Database connection error:', error);
+    process.exit(1); // Exit process with failure
   }
-
-}
+};
 
 module.exports = connectDB;
